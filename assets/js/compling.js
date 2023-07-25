1605d16cb0ba1602b90b552505fc2949e6fe7c1d
aykae <raiaboudi@yahoo.com>
2023-07-25 16:32:43 -0400

    embellishing compling page

--- assets/js/compling.js
+++ assets/js/compling.js
@@ -2,7 +2,10 @@ const genBtn = document.querySelector("#genBtn");
 
 genBtn.addEventListener("click", async () => {
     try {
-        const response = await fetch("http://127.0.0.1:8000/clapps/gensent/");
+        const response = await fetch("http://127.0.0.1:8000/clapps/gensent/",
+        {
+            method: "GET",
+        });
         const sentObj = await response.json();
         console.log(sentObj);
         
